5a32b82d312b76c9d0100d52887852ff3520aca7
patricioPascual <patriciopascual2@gmail.com>
2026-06-05 13:40:17 -0300

    Initial commit

---
+++ src/ingtestunit/ConversorDeMoneda.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ingtestunit;
+
+/**
+ *
+ * @author patri
+ */
+public class ConversorDeMoneda {
+ 
+    
+    
+    
+  public static double convertir(double cantidad, double cotizacion) {
+        return cantidad * cotizacion;
+    }
+
+
+public static double calcularCotizacion(double cantidadEntregada, double cantidadRecibida) {
+        if (cantidadRecibida == 0) {
+            throw new IllegalArgumentException("La cantidad recibida no puede ser cero.");
+        }
+        return cantidadEntregada / cantidadRecibida;
+    }
+
+
+
+public static double aumentarSaldo(double saldoActual, double montoComprado) {
+        return saldoActual + montoComprado;
+    }
+    
+
+
+public static void simularRetiroFisico(double saldoTotal, int denominacion) {
+        if (denominacion <= 0) {
+            System.out.println("La denominación del billete debe ser mayor a cero.");
+            return;
+        }
+
+        int cantidadBilletes = (int) (saldoTotal / denominacion);
+     
+        double saldoRestante = saldoTotal % denominacion;
+
+        System.out.println("--- Simulación de Retiro Físico ---");
+        System.out.println("Saldo total a retirar: U$S " + saldoTotal);
+        System.out.println("Billetes entregados (" + denominacion + "): " + cantidadBilletes);
+        System.out.println("Saldo restante en cuenta: U$S " + saldoRestante);
+    }
+}
+
