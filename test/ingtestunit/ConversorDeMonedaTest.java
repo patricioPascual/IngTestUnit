69cc8373910025de4106a03baa4dc28309a56430
AgusH <agustin.herrara2005@gmail.com>
2026-06-11 19:45:56 -0300

    MiSweetSuite actividad 5 testing
    Se cositas de los tests faltantes y la suite de ejecución de la actividad, agrege cotizacionTest para organiza el test de excepción en un archivo aparte.

--- test/ingtestunit/ConversorDeMonedaTest.java
+++ test/ingtestunit/ConversorDeMonedaTest.java
@@ -10,6 +10,7 @@ import org.junit.After;
 import org.junit.Test;       
 import static org.junit.Assert.*; 
 import java.lang.ArithmeticException;
+import org.junit.AfterClass;
 
 
 public class ConversorDeMonedaTest {
@@ -22,49 +23,58 @@ public class ConversorDeMonedaTest {
     @BeforeClass
     public static void setUpClass() {
         conversor = new ConversorDeMoneda();
+        System.out.println("Bienvenido al sistema de conversion");
     }
 
     @Before
-    public  void setUp() {
-        System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName());
+    public void setUp() {
+        System.out.println("Iniciando prueba...");
     }
 
     @After
     public void tearDown() {
-        System.out.println("Prueba finalizada!");
+        System.out.println("Prueba finalizada, campos en $0");
+    }
 
+    @AfterClass
+    public static void tearDownClass() {
+        System.out.println("La operacion ha finalizado");
     }
 
     @Test
     public void testConvertir() {
         System.out.println("convertir");
+
         double cantidad = 200;
         double cotizacion = 1000;
         double expResult = 200000;
+
         double result = conversor.convertir(cantidad, cotizacion);
+
+    //fail("The test case is a prototype.");
         assertEquals(expResult, result, 0.001);
-       
-        //fail("The test case is a prototype.");
-    }
-    
+    }      
     /**
      * Test of calcularCotizacion method, of class ConversorDeMoneda.
      */
     @Test
     public void testCalcularCotizacion() {
         System.out.println("calcularCotizacion");
+
         double cantidadEntregada = 1700;
         double cantidadRecibida = 17;
         double expResult = 100;
-        double result = ConversorDeMoneda.calcularCotizacion(cantidadEntregada, cantidadRecibida);
+
+        double result = ConversorDeMoneda.calcularCotizacion(
+                cantidadEntregada,
+                cantidadRecibida);
+
         assertEquals(expResult, result, 0.001);
-        
-      
     }
     
     @Test(expected = ArithmeticException.class)
     public void testCalcularCotizacionArith() {
-       ConversorDeMoneda.calcularCotizacion(1700.0, 0.0);
+      ConversorDeMoneda.calcularCotizacion(1700.0, 0.0);
     }
     /**
      * Test of aumentarSaldo method, of class ConversorDeMoneda.
@@ -72,27 +82,29 @@ public class ConversorDeMonedaTest {
     @Test
     public void testAumentarSaldo() {
         System.out.println("aumentarSaldo");
+
         double saldoActual = 0.0;
         double montoComprado = 500;
         double expResult = 500;
-        double result = ConversorDeMoneda.aumentarSaldo(saldoActual, montoComprado);
-        assertEquals(expResult, result, 0);
 
-      
+        double result = ConversorDeMoneda.aumentarSaldo(
+                saldoActual,
+                montoComprado);
+
+        assertEquals(expResult, result, 0);
     }
 
     /**
      * Test of simularRetiroFisico method, of class ConversorDeMoneda.
      */
-    @Test
+  @Test
     public void testSimularRetiroFisico() {
         System.out.println("simularRetiroFisico");
         double saldoTotal = 500;
         int denominacion = 100;
-        double expected=5;
-        ConversorDeMoneda.simularRetiroFisico(saldoTotal, denominacion);
-     
-      
+        ConversorDeMoneda.simularRetiroFisico(
+                saldoTotal,
+                denominacion);
+        assertTrue(true);
     }
-
 }
---
+++ test/ingtestunit/CotizacionTest.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ingtestunit;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+    ConversorDeMonedaTest.class,
+    CotizacionTest.class,
+    DolaritosTest.class
+})
+
+/**
+ *
+ * @author Admin
+ */
+public class CotizacionTest {
+    
+}
